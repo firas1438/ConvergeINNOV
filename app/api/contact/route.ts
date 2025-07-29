83c5a32551ba7e6d9f971bde7d81bbf91c03d0be
Firas Ben Ali <benalifiras1438@gmail.com>
2025-07-29 02:42:03 +0100

    fixed bugs

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,7 +3,7 @@ import { connectDB } from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 // GET method (fetch contact data)
-export async function GET(request: NextRequest) {
+export async function GET(_request: NextRequest) {
   await connectDB();
 
   try {
